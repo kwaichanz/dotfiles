c561901e79d629f96ff3d57e8d9feffa78151536
Kwanchai <alphezelphaz@gmail.com>
2025-04-08 15:48:09 +0700

    nvim: Updated lock versions

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -79,4 +79,29 @@ return {
       "RainbowMultiDelim",
     },
   },
+  -- {
+  --   "fredrikaverpil/godoc.nvim",
+  --   version = "*",
+  --   dependencies = {
+  --     { "nvim-telescope/telescope.nvim" }, -- optional
+  --     { "folke/snacks.nvim" }, -- optional
+  --     { "echasnovski/mini.pick" }, -- optional
+  --     { "ibhagwan/fzf-lua" }, -- optional
+  --     {
+  --       "nvim-treesitter/nvim-treesitter",
+  --       opts = {
+  --         ensure_installed = { "go" },
+  --       },
+  --     },
+  --   },
+  --   build = "go install github.com/lotusirous/gostdsym/stdsym@latest", -- optional
+  --   cmd = { "GoDoc" }, -- optional
+  --   opts = {}, -- see further down below for configuration
+  --   picker = {
+  --     type = "snacks",
+  --
+  --     snacks = {},
+  --     telescope = {},
+  --   },
+  -- },
 }
